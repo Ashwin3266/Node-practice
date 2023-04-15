576f09fa150a9605a97e733f1f0e5cdead6d1e86
Ashwin <tsashwin878@gmail.com>
2023-04-15 16:33:28 +0530

    Files are added successfully

--- 11-Replacing-Promises-with-callbacks.js
+++ 11-Replacing-Promises-with-callbacks.js
@@ -4,9 +4,10 @@ const start=async()=>{
     try{
         const first=await readFile('./content/first.txt','utf-8')
         const second=await readFile('./content/second.txt','utf-8')
-        await writeFile('./content/result-async.txt',
+        const result=await writeFile('./content/result-async.txt',
                         `Callbacks are changed to promises :\n ${first} \n ${second} \n\n`,
                         {flag:'a'})
+        console.log(result);
     }
     catch(error)
     {
